d76a0f2241b8378298d861ab0e57c11457980d96
Bálint Boda <bb20020402@gmail.com>
2026-08-21 23:48:27 +0200

    update

--- packages/@valentinusz/oxlint/react/vite.config.ts
+++ packages/@valentinusz/oxlint/react/vite.config.ts
@@ -1,4 +1,3 @@
-import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import dts from 'unplugin-dts/vite';
 import { defineConfig } from 'vite';
@@ -6,7 +5,7 @@ import { defineConfig } from 'vite';
 import packageJson from './package.json' with { type: 'json' };
 
 export default defineConfig({
-  plugins: [dts(), react()],
+  plugins: [dts()],
   build: {
     emptyOutDir: true,
     lib: {
